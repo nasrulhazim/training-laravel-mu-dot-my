92b0bde8db1be7aa93edd255390102438afceec9
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-12-19 14:31:02 +0800

    Added Laravel Permission

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -12,7 +12,7 @@ class UserPolicy
      */
     public function viewAny(User $user): bool
     {
-        return true;
+        return $user->can('view-all-users');
     }
 
     /**
@@ -20,7 +20,7 @@ class UserPolicy
      */
     public function view(User $user, User $model): bool
     {
-        return $user->id == $model->id;
+        return $user->can('view-user') || $user->id == $model->id;
     }
 
     /**
@@ -28,7 +28,7 @@ class UserPolicy
      */
     public function create(User $user): bool
     {
-        return true;
+        return $user->can('create-user');
     }
 
     /**
@@ -36,7 +36,7 @@ class UserPolicy
      */
     public function update(User $user, User $model): bool
     {
-        return $user->id == $model->id;
+        return $user->can('update-user') || $user->id == $model->id;
     }
 
     /**
@@ -44,7 +44,7 @@ class UserPolicy
      */
     public function delete(User $user, User $model): bool
     {
-        return $user->id != $model->id;
+        return $user->can('delete-user') && $user->id != $model->id;
     }
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +17,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->prepare();
+
         $this->call(DummySeeder::class);
+    }
+
+    private function prepare()
+    {
+        $superadmin = Role::updateOrCreate([
+            'name' => 'Superadmin',
+        ]);
+
+        $permissions = [
+            'view-all-users',
+            'view-user',
+            'create-user',
+            'update-user',
+            'delete-user',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::updateOrCreate([
+                'name' => $permission,
+            ]);
+            $superadmin->givePermissionTo($permission);
+        }
+
+        $user = Role::updateOrCreate([
+            'name' => 'User',
+        ]);
+
+        $superuser = User::updateOrCreate([
+            'email' => 'super@app.com',
+        ],[
+            'name' => 'superadmin',
+            'password' => Hash::make('password'),
+        ]);
+
+        $superuser->assignRole($superadmin);
     }
 }
